8f8cca8248a1a7fd24d4f8bd9e36a08266a3be93
Shubham <shubhamsinghmau2@gmail.com>
2023-09-09 11:36:00 +0530

    multicolored ball under gravity on mouse click

---
+++ DAY16/main.js
@@ -0,0 +1,138 @@
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.shadowMap.enabled = true;
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+);
+camera.position.set(0, 5, 10);
+
+const directionalLight = new THREE.DirectionalLight('white', 0.8);
+directionalLight.position.set(0, 10, 0);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+scene.add(directionalLight);
+
+
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+const planeGeo = new THREE.PlaneGeometry(10, 10, 30, 30);
+const planeMat = new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
+    color: 'white'
+});
+const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+// planeMesh.rotation.x = Math.PI / 180 * 90;
+// planeMesh.rotation.y = Math.PI / 180 * 45;
+scene.add(planeMesh);
+planeMesh.receiveShadow = true;
+
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const mouse = new THREE.Vector2();
+const intersectionPoint = new THREE.Vector3();
+const planeNormal = new THREE.Vector3();
+const plane = new THREE.Plane();
+const raycaster = new THREE.Raycaster();
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    planeNormal.copy(camera.position).normalize();
+    // plane.setFromNormalAndCoplanarPoint(planeNormal, new THREE.Vector3(0, 0, 0)); //unitVector, originPoint
+    plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+    raycaster.setFromCamera(mouse, camera);
+    raycaster.ray.intersectPlane(plane, intersectionPoint);
+})
+
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.81, 0),
+});
+
+const groundMaterial = new CANNON.Material();
+
+const groundBody = new CANNON.Body({
+    shape: new CANNON.Box(new CANNON.Vec3(5, 5, 0.01)),
+    type: CANNON.Body.STATIC,
+    material: groundMaterial,
+
+});
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(groundBody);
+
+let sphereMeshArray = [];
+let sphereBodyArray = [];
+
+window.addEventListener('click', function (e) {
+    const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
+    const sphereMat = new THREE.MeshStandardMaterial({
+        color: Math.random() * 0xFFFFFF,
+        roughness: 0,
+        metalness: 0.2
+    });
+    const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+    sphereMesh.castShadow = true;
+    const sphereMaterial = new CANNON.Material();
+    const sphereBody = new CANNON.Body({
+        mass: 3,
+        shape: new CANNON.Sphere(0.125),
+        material: sphereMaterial,
+        position: new CANNON.Vec3(intersectionPoint.x, intersectionPoint.y, intersectionPoint.z)
+    });
+    scene.add(sphereMesh);
+    sphereBodyArray.push(sphereBody);
+    sphereMeshArray.push(sphereMesh);
+
+    world.addBody(sphereBody);
+    sphereBody.linearDamping = 0.4;
+
+    const groundSphereContactMat = new CANNON.ContactMaterial(
+        groundMaterial,
+        sphereMaterial,
+        { restitution: 0.9 }
+    );
+    world.addContactMaterial(groundSphereContactMat);
+})
+
+
+const timeStep = 1 / 60;
+
+function animate() {
+
+    world.step(timeStep);
+    planeMesh.position.copy(groundBody.position);
+    planeMesh.quaternion.copy(groundBody.quaternion);
+
+    for (let i = 0; i < sphereBodyArray.length; i++) {
+        sphereMeshArray[i].position.copy(sphereBodyArray[i].position);
+        sphereMeshArray[i].quaternion.copy(sphereBodyArray[i].quaternion);
+    }
+
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+
+
